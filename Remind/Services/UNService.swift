d1cd01c87b4768ff5d3680183f2c714f2ed4624e
Wisnu Anggoro <wisnu@anggoro.net>
2018-06-21 13:02:57 +0700

    Setup UserNotifications protocol

---
+++ Remind/Services/UNService.swift
@@ -0,0 +1,51 @@
+//
+//  UNService.swift
+//  Remind
+//
+//  Created by MTMAC18 on 21/06/18.
+//  Copyright © 2018 Wisnu Anggoro. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class UNService: NSObject {
+    
+    private override init() {}
+    static let shared = UNService()
+    
+    let unCenter = UNUserNotificationCenter.current()
+    
+    func authorize() {
+        let options : UNAuthorizationOptions = [.alert, .badge, .sound, .carPlay]
+        unCenter.requestAuthorization(options: options) { (granted, error) in
+            print(error ?? "No UN auth error")
+            
+            guard granted else {
+                print("USER DENIED ACCESS")
+                return
+            }
+            
+            self.configure()
+        }
+    }
+    
+    func configure() {
+        unCenter.delegate = self
+    }
+}
+
+extension UNService: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("UN did receive response")
+        
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("UN will present")
+        
+        let options: UNNotificationPresentationOptions = [.alert, .sound]
+        completionHandler(options)
+    }
+}
